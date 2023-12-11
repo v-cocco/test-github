5a2bcbf8b43f2ca02688f50eb33530a926fe616f
Valentin Cocco <cocco.valentin@hotmail.fr>
2023-12-11 10:50:35 +0100

    added message to user

--- maketable.sh
+++ maketable.sh
@@ -1,2 +1,3 @@
+echo hello user, will make a contigency table now.
 cd ~/gasprices   # make sure we are in the right place
 cut -d ';' -f 5 carburants.csv | tr [:lower:] [:upper:] | sort | uniq -c | sort
