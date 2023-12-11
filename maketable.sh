3e53f8553a787a755014c486ca6c7b3a19c9b597
Valentin Cocco <cocco.valentin@hotmail.fr>
2023-12-11 11:09:15 +0100

    added end message to script

--- maketable.sh
+++ maketable.sh
@@ -1,3 +1,4 @@
 echo hello user, will make a contigency table now.
 cd ~/gasprices   # make sure we are in the right place
 cut -d ';' -f 5 carburants.csv | tr [:lower:] [:upper:] | sort | uniq -c | sort
+echo program ran successfully
